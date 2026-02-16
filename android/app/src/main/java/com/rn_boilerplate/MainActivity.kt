fc3937f3c1bb4a2b2fae5694e4c25c77fab4c0d6
AbdullahAnsariSalsoft6668 <abdullahansarisalsoft@gmail.com>
2026-02-16 18:54:07 +0500

    feat: Rename app to eveWear, update package identifiers, and configure Yarn node linker.

--- android/app/src/main/java/com/rn_boilerplate/MainActivity.kt
+++ android/app/src/main/java/com/rn_boilerplate/MainActivity.kt
@@ -1,4 +1,4 @@
-package boilerplate.expo
+package com.mapps.eveWear
 
 import android.os.Bundle
 
--- android/app/src/main/java/com/rn_boilerplate/MainApplication.kt
+++ android/app/src/main/java/com/rn_boilerplate/MainApplication.kt
@@ -1,4 +1,4 @@
-package boilerplate.expo
+package com.mapps.eveWear
 
 import android.app.Application
 import com.facebook.react.PackageList
